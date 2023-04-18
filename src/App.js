a6668b74de5f1ccf8b15fb084048295c9e1645da
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-04-18 18:26:17 +0300

    react props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    { title: "car Insurance", amount: 294.67, date: new Date(2023, 4, 12) },
+    { title: "car Insurance 2", amount: 294.68, date: new Date(2023, 4, 18) },
+    { title: "car Insurance", amount: 294.67, date: new Date(2023, 4, 12) },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
     </div>
   );
 }
