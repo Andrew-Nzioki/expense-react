658f80d3a90f14ad399c4ce622e75aa6a798828d
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-04-20 06:43:46 +0300

    complete project

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,45 +1,28 @@
-import React,{useState} from "react";
-import ExpenseItem from "./ExpenseItem";
+import React, { useState } from "react";
 import "./Expenses.css";
-import ExpensesFilter from './ExpensesFilter';
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  
   return (
-
     <Card className="expenses">
-       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {props.items.map((expense) => (
-      
-        <ExpenseItem
-        key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          id={expense.id}
-        />
-      ))}
-
-
-      {/* <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      /> */}
+      <ExpensesChart expenses={filteredExpenses }/>
+      <ExpensesList items={filteredExpenses}/> 
     </Card>
   );
 }
